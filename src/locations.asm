f141689a128c2a6e4f5c33dd8b7471b239b661cf
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2012-10-11 00:41:02 +0000

    Modifying map_data.asm to use more appropriate scope trickery to allow map data generation to contain the same symbols in every map. Previously, we had repeatedly included the same include files inside a generic scope---unfortunately this had the side effect of re-defining the define_location macro wherever it was used.
    So, we've also moved define_location back into locations.inc where it belongs.
    
    This has a nice by product of being able to select all and paste when updating any map data.
    
    git-svn-id: svn://localhost/Owlia/trunk@67 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/locations.asm
+++ src/locations.asm
@@ -16,22 +16,6 @@ locations_hi:
 ;****************************************************************
 ;Location definitions.
 ;****************************************************************
-.macro define_location area_index, camera_start_x, camera_start_y, hero_offset_x, hero_offset_y, sfx_address, sfx_channel, sfx_stream
-.scope
-  .byte area_index                              ;area_index .byte
-  .byte $20 | ((>(16*camera_start_x) & 1) << 2) ;nametable_start_hibyte .byte
-  .word (16*camera_start_x)                     ;camera_start_x .word
-  .word (16*camera_start_y)                     ;camera_start_y .word
-  .byte <(16*camera_start_x)                    ;camera_start_scroll_x .byte
-  .byte (232 + (16*camera_start_y)) .MOD 240    ;camera_start_scroll_y .byte
-  .word (16*camera_start_x+hero_offset_x)       ;hero_start_x .word
-  .word (16*camera_start_y+hero_offset_y)       ;hero_start_y .word
-  .word sfx_address                             ;on_enter_sfx_address .word
-  .byte sfx_channel                             ;on_enter_sfx_channel .byte
-  .byte sfx_stream                              ;on_enter_sfx_stream  .byte
-.endscope
-.endmacro
-
 village_area_house1_entrance_location:
 define_location area_index_village, 8, 6, 128, 136, sfx_door, 3, soundeffect_one
 
--- src/map_data.asm
+++ src/map_data.asm
@@ -1,27 +1,59 @@
-.segment "ROM02"
-
-;this macro is used to maintain in one place all the includes
-;that are needed for all areas in the game.
-.macro area_includes
-
 .include "map_data.inc"
 .include "map.inc"
 .include "areas.inc"
 .include "locations.inc"
 
-.endmacro
+.segment "ROM02"
 
-.scope
-area_includes
+.scope village_data
 .include "village.inc"
 .endscope
+village_palette = village_data::palette
+village_metatile_table_properties = village_data::metatile_table_properties
+village_metatile_table_params = village_data::metatile_table_params
+village_metatile_table_attributes = village_data::metatile_table_attributes
+village_metatile_table_top_left_tiles = village_data::metatile_table_top_left_tiles
+village_metatile_table_top_right_tiles = village_data::metatile_table_top_right_tiles
+village_metatile_table_bottom_left_tiles = village_data::metatile_table_bottom_left_tiles
+village_metatile_table_bottom_right_tiles = village_data::metatile_table_bottom_right_tiles
+village_big_metatile_table_top_left = village_data::big_metatile_table_top_left
+village_big_metatile_table_top_right = village_data::big_metatile_table_top_right
+village_big_metatile_table_bottom_left = village_data::big_metatile_table_bottom_left
+village_big_metatile_table_bottom_right = village_data::big_metatile_table_bottom_right
+village_map = village_data::map
 
-.scope
-area_includes
+.scope house1_data
 .include "house1.inc"
 .endscope
 
-.scope
-area_includes
+house1_palette = house1_data::palette
+house1_metatile_table_properties = house1_data::metatile_table_properties
+house1_metatile_table_params = house1_data::metatile_table_params
+house1_metatile_table_attributes = house1_data::metatile_table_attributes
+house1_metatile_table_top_left_tiles = house1_data::metatile_table_top_left_tiles
+house1_metatile_table_top_right_tiles = house1_data::metatile_table_top_right_tiles
+house1_metatile_table_bottom_left_tiles = house1_data::metatile_table_bottom_left_tiles
+house1_metatile_table_bottom_right_tiles = house1_data::metatile_table_bottom_right_tiles
+house1_big_metatile_table_top_left = house1_data::big_metatile_table_top_left
+house1_big_metatile_table_top_right = house1_data::big_metatile_table_top_right
+house1_big_metatile_table_bottom_left = house1_data::big_metatile_table_bottom_left
+house1_big_metatile_table_bottom_right = house1_data::big_metatile_table_bottom_right
+house1_map = house1_data::map
+
+.scope overworld_data
 .include "overworld.inc"
 .endscope
+
+overworld_palette = overworld_data::palette
+overworld_metatile_table_properties = overworld_data::metatile_table_properties
+overworld_metatile_table_params = overworld_data::metatile_table_params
+overworld_metatile_table_attributes = overworld_data::metatile_table_attributes
+overworld_metatile_table_top_left_tiles = overworld_data::metatile_table_top_left_tiles
+overworld_metatile_table_top_right_tiles = overworld_data::metatile_table_top_right_tiles
+overworld_metatile_table_bottom_left_tiles = overworld_data::metatile_table_bottom_left_tiles
+overworld_metatile_table_bottom_right_tiles = overworld_data::metatile_table_bottom_right_tiles
+overworld_big_metatile_table_top_left = overworld_data::big_metatile_table_top_left
+overworld_big_metatile_table_top_right = overworld_data::big_metatile_table_top_right
+overworld_big_metatile_table_bottom_left = overworld_data::big_metatile_table_bottom_left
+overworld_big_metatile_table_bottom_right = overworld_data::big_metatile_table_bottom_right
+overworld_map = overworld_data::map
