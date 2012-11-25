bfa7d43960f135f394f246b0287668788ddaacb1
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2012-11-25 18:54:15 +0000

    Cleaning up sfx data volume envelope names etc. to be easier to figure out indices, etc.
    git-svn-id: svn://localhost/Owlia/trunk@107 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/sfx_data.asm
+++ src/sfx_data.asm
@@ -9,14 +9,16 @@ sfx_volume_envelope_silence:
 sfx_volume_envelope_loud:
   .byte 15, ENV_STOP
 
-sfx_volume_envelope_1:
+;used for hit
+sfx_volume_envelope_2:
   .byte 14, 12, 11, 9, 7, 6, 4, 2, 1, 0, 0, ENV_STOP
 
-sfx_volume_envelope_2:
+;used for door slam
+sfx_volume_envelope_3:
   .byte 10,7,5,4,0,ENV_STOP
 
-;flapping sound
-sfx_volume_envelope_3:
+;used for flapping
+sfx_volume_envelope_4:
   .byte 3,5,9,12,13,10,6,3,5,7,8,9,8,6,0,ENV_LOOP
 
 sfx_pitch_envelope_0:
@@ -33,9 +35,9 @@ sfx_duty_envelope_1:
 sfx_volume_envelopes:
   .word sfx_volume_envelope_silence
   .word sfx_volume_envelope_loud
-  .word sfx_volume_envelope_1
   .word sfx_volume_envelope_2
   .word sfx_volume_envelope_3
+  .word sfx_volume_envelope_4
 
 sfx_pitch_envelopes:
   .word sfx_pitch_envelope_0
@@ -51,11 +53,11 @@ sfx_set1:
   .word sfx_duty_envelopes
 
 sfx_test:
-  .byte STV, 2, STP, 0, SDU, 0, STL, 50, 5
+  .byte STV,2, STP, 0, SDU, 0, STL, 50, 5
   .byte TRM
 
 sfx_door:
-  .byte STV, 3,STP,0,SDU,0,STL,6,10,15
+  .byte STV,3,STP,0,SDU,0,STL,6,10,15
   .byte TRM
 
 sfx_flap:
