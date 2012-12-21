b39f44c21b394edb48a3f919dd4d56df76befc94
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2012-12-21 01:32:39 +0000

    Adding inside room graphics. Trimming village graphics of tiles we won't use for the demo (to make room for font). Trimming textbox graphics to make room for as much as what we had of the village and inside room graphics as we can. After trimming the textbox, we didn't need to trim the inside house graphics. Removing equates for numerals and some punctuation that we felt we would never need in NPC conversations. Modifying test conversations to remove use of a numeral.
    git-svn-id: svn://localhost/Owlia/trunk@124 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/conversation_data.asm
+++ src/conversation_data.asm
@@ -19,7 +19,7 @@ conversations_hi:
 test_conversation:
   .byte 1, H,E,L,L,O,SP,T,H,E,R,E,EN,WT,EL
   .byte 2, I,SP,_A,M,SP,_A,SP,B,U,T,T,EN,WT,EL
-  .byte 3, _Y,O,U,SP,_A,R,E,SP,_A,SP,B,U,T,T,SP,_2,WT,EP,EL
+  .byte 3, _Y,O,U,SP,_A,R,E,SP,_A,SP,B,U,T,T,SP,T,O,O,CA,WT,EP,EL
   .byte 1, W,E,SP,_A,R,E,SP,B,O,T,H,SP,B,U,T,T,S,PD,WT,EC
 
 another_conversation:
