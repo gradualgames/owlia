ff224cfa598f0989deb285b1f02fbda3a855e089
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2013-01-28 00:37:37 +0000

    Grouping all locations with their respective areas in preparation for possible further refactoring for ease of editing.
    git-svn-id: svn://localhost/Owlia/trunk@212 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/locations.asm
+++ src/locations.asm
@@ -12,24 +12,24 @@
 ;Location LUTs
 ;****************************************************************
 .define locations \
-    village_area_house1_entrance_location, \
-    house1_area_exit_location, \
-    village_area_inn_entrance_location, \
-    inn_area_exit_location, \
-    village_area_weaponstore_entrance_location, \
-    weaponstore_area_exit_location, \
-    village_area_potionstore_entrance_location, \
-    potionstore_area_exit_location, \
-    village_area_housebl_entrance_location, \
-    housebl_area_exit_location, \
-    village_area_housebr_entrance_location, \
-    housebr_area_exit_location, \
-    village_area_housetr_entrance_location, \
-    housetr_area_exit_location, \
-    village_area_bottom_entrance_location, \
-    overworld_area_top_entrance_location, \
-    dungeon_area_start_location, \
-    dungeon_entrance_location, \
+    village_house1_entrance_location, \
+    village_inn_entrance_location, \
+    village_weaponstore_entrance_location, \
+    village_potionstore_entrance_location, \
+    village_housebl_entrance_location, \
+    village_housebr_entrance_location, \
+    village_housetr_entrance_location, \
+    village_bottom_entrance_location, \
+    house1_exit_location, \
+    inn_exit_location, \
+    weaponstore_exit_location, \
+    potionstore_exit_location, \
+    housebl_exit_location, \
+    housebr_exit_location, \
+    housetr_exit_location, \
+    overworld_top_entrance_location, \
+    overworld_dungeon_entrance_location, \
+    dungeon_start_location, \
     dungeon_room_a_location, \
     dungeon_room_b_location
 
@@ -43,59 +43,69 @@ locations_hi:
 ;****************************************************************
 ;Location definitions.
 ;****************************************************************
-village_area_house1_entrance_location:
+
+;village locations
+village_house1_entrance_location:
 define_centered_location 0, area_index_village, 11, 14, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
 
-house1_area_exit_location:
-define_centered_location 0, area_index_house, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-village_area_inn_entrance_location:
+village_inn_entrance_location:
 define_centered_location 0, area_index_village, 11, 35, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
 
-inn_area_exit_location:
-define_centered_location 0, area_index_inn, 25, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-village_area_weaponstore_entrance_location:
+village_weaponstore_entrance_location:
 define_centered_location 0, area_index_village, 54, 35, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
 
-weaponstore_area_exit_location:
-define_centered_location 0, area_index_store, 25, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-village_area_potionstore_entrance_location:
+village_potionstore_entrance_location:
 define_centered_location 0, area_index_village, 51, 35, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
 
-potionstore_area_exit_location:
-define_centered_location 0, area_index_store, 13, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-village_area_housebl_entrance_location:
+village_housebl_entrance_location:
 define_centered_location 0, area_index_village, 11, 55, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
 
-housebl_area_exit_location:
-define_centered_location 0, area_index_housebl, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-village_area_housebr_entrance_location:
+village_housebr_entrance_location:
 define_centered_location 0, area_index_village, 51, 55, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
 
-housebr_area_exit_location:
-define_centered_location 0, area_index_housebr, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-village_area_housetr_entrance_location:
+village_housetr_entrance_location:
 define_centered_location 0, area_index_village, 51, 14, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
 
-housetr_area_exit_location:
-define_centered_location 0, area_index_housetr, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-village_area_bottom_entrance_location:
+village_bottom_entrance_location:
 define_south_location 0, area_index_village, 31, 62, 0, 0, 0, HERO_DIRECTION_UP
 
-overworld_area_top_entrance_location:
+;house1 locations
+house1_exit_location:
+define_centered_location 0, area_index_house, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
+
+;inn locations
+inn_exit_location:
+define_centered_location 0, area_index_inn, 25, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
+
+;store locations
+weaponstore_exit_location:
+define_centered_location 0, area_index_store, 25, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
+
+potionstore_exit_location:
+define_centered_location 0, area_index_store, 13, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
+
+;housebl locations
+housebl_exit_location:
+define_centered_location 0, area_index_housebl, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
+
+;housebr locations
+housebr_exit_location:
+define_centered_location 0, area_index_housebr, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
+
+;housetr locations
+housetr_exit_location:
+define_centered_location 0, area_index_housetr, 14, 25, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
+
+;overworld locations
+overworld_top_entrance_location:
 define_north_location 0, area_index_overworld, 27, 1, 0, 0, 0, HERO_DIRECTION_DOWN
 
-dungeon_area_start_location:
-define_location 0, area_index_dungeon, 0, 2, 7, 9, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
-
-dungeon_entrance_location:
+overworld_dungeon_entrance_location:
 define_centered_location 0, area_index_overworld, 30, 13, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
+
+;dungeon locations
+dungeon_start_location:
+define_location 0, area_index_dungeon, 0, 2, 7, 9, sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
 
 dungeon_room_a_location:
 define_location LOCATION_FLAGS_IS_LOCKED_SET, area_index_dungeon, 0, 0, 7, 2, sfx_door, 3, soundeffect_one, HERO_DIRECTION_DOWN
