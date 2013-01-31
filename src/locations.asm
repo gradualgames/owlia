79601324aaa344fff573eef9082783b0bdf1b948
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2013-01-31 01:25:44 +0000

    Fixing up one of the test dungeon locations.
    git-svn-id: svn://localhost/Owlia/trunk@218 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/locations.asm
+++ src/locations.asm
@@ -120,8 +120,8 @@ define_location LOCATION_FLAGS_IS_LOCKED_SET | \
                 HERO_DIRECTION_DOWN
 
 dungeon_room_b:
-define_centered_location LOCATION_FLAGS_IS_LOCKED_SET | \
+define_location LOCATION_FLAGS_IS_LOCKED_SET | \
                          LOCATION_FLAGS_CAMERA_X_SCROLLING_DISABLED_SET | \
                          LOCATION_FLAGS_CAMERA_Y_SCROLLING_DISABLED_SET, \
-                         area_index_dungeon, 23, 10, \
+                         area_index_dungeon, 16, 0, 23, 10, \
                          sfx_door, 3, soundeffect_one, HERO_DIRECTION_UP
