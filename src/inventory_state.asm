b518403478c9631ea5b7a60b592053af3719b0dd
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2013-03-09 20:31:55 +0000

    Moving much of the inventory state's implementation into a swappable bank. It was getting kind of large and we were beginning to get a little too low on fixed bank space for comfort.
    git-svn-id: svn://localhost/Owlia/trunk@270 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/inventory_state.asm
+++ src/inventory_state.asm
@@ -19,6 +19,247 @@
 
 .segment "CODE"
 
+inventory_screen_palette:
+  .byte $0d,$0d,$18,$20,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+  .byte $0d,$0d,$0d,$20,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+
+inventory_state_init:
+
+  ;fade out from current palette
+  switch_bank_ldy #AREAS_BANK
+  ldy #area::palette_address
+  lda (area_address),y
+  sta w0
+  iny
+  lda (area_address),y
+  sta w0+1
+  switch_bank_ldy map_bank
+  jsr ppu_fade_out_palette
+
+  ;set blank nmi routine
+  lda #<ppu_vblank_nop
+  sta vblank_routine
+  lda #>ppu_vblank_nop
+  sta vblank_routine+1
+
+  jsr ppu_safely_disable_graphics
+
+  jsr sprite_clear_all
+
+  jsr sprite_update_all
+
+  ;load chr data for inventory screen
+  lda #$00
+  sta $2006
+  sta $2006
+
+  ;reset tile accumulator
+  lda #$00
+  sta b3
+
+  lda #<inventory_chr
+  sta w0
+  lda #>inventory_chr
+  sta w0+1
+  switch_bank_ldy #INVENTORY_STATE_BG_CHR_BANK
+  jsr ppu_load_chr_amount
+
+  ;grab tile accumulator to know where the textbox and font group begins
+  lda b3
+  sta textbox_and_font_chr_offset
+
+  ;load the textbox graphics. This is hardcoded because it is the same
+  ;for the entire game. The assumption here is that the background
+  ;graphics we use will never occupy so many tiles that we cannot
+  ;display a textbox or font.
+  lda #<textbox_chr
+  sta w0
+  lda #>textbox_chr
+  sta w0+1
+  switch_bank_ldy #TEXTBOX_BG_CHR_BANK
+  jsr ppu_load_chr_amount
+
+  ;load cursor graphics
+  lda #$10
+  sta $2006
+  lda #$00
+  sta $2006
+
+  ;reset tile accumulator
+  lda #$00
+  sta b3
+
+  lda #<Cursor_chr
+  sta w0
+  lda #>Cursor_chr
+  sta w0+1
+
+  ;store the current chr offset in sprite_chr_groups_chr_offsets array for the cursor
+  ldy #sprite_chr_group_index_cursor
+  lda b3
+  sta sprite_chr_group_offsets,y
+
+  switch_bank_ldy #INVENTORY_STATE_SPRITE_CHR_BANK
+  jsr ppu_load_chr_amount
+
+  ;load nametable data for inventory screen
+  ;load the nametable and attribute table.
+  lda #$20
+  sta ppu_2006
+  lda #$00
+  sta ppu_2006+1
+  upload_ppu_2006
+  lda #<inventory_screen
+  sta w0
+  lda #>inventory_screen
+  sta w0+1
+  switch_bank_ldy #INVENTORY_STATE_BG_NAMETABLE_BANK
+  jsr ppu_load_nametable
+
+  ;reset scroll
+  lda #$20
+  sta ppu_2006
+  lda #$00
+  sta ppu_2006
+  upload_ppu_2006
+
+  lda #0
+  sta ppu_2005
+  sta ppu_2005+1
+  upload_ppu_2005
+
+  ;initialize and draw initial cursor
+  lda #menu_position_health
+  sta state_control_params+inventory_state_control::menu_position
+
+  ldx state_control_params+inventory_state_control::menu_position
+  switch_bank_ldy #INVENTORY_STATE_BANK
+  lda menu_position_row,x
+  sta state_control_params+inventory_state_control::cursor_row
+  lda menu_position_column,x
+  sta state_control_params+inventory_state_control::cursor_column
+
+  jsr draw_cursor
+
+  jsr sprite_update_all
+
+  ;draw menu layout with strings
+  switch_bank_ldy #INVENTORY_STATE_BANK
+  print_string inventory_string, #$20, #4, #11
+
+  print_string gp_string, #$20, #7, #4
+  print_string keys_string, #$20, #8, #4
+
+  print_string use_item_string, #$20, #10, #4
+  print_string health_string, #$20, #10, #13
+  print_string owl_health_string, #$20, #11, #13
+  print_string rope_string, #$20, #12, #13
+
+  print_string carry_string, #$20, #14, #4
+  print_string bomb_string, #$20, #14, #13
+  print_string lantern_string, #$20, #15, #13
+
+  print_string tech_string, #$20, #17, #4
+  print_string rush_string, #$20, #17, #13
+  print_string fetch_string, #$20, #18, #13
+  print_string sonar_string, #$20, #19, #13
+  print_string carry_item_string, #$20, #20, #13
+  print_string carry_adlanniel_string, #$20, #21, #13
+  print_string confuse_string, #$20, #22, #13
+  print_string homing_string, #$20, #23, #13
+  print_string multi_homing_string, #$20, #24, #13
+
+  jsr ppu_safely_enable_graphics
+
+  ;fade in inventory screen palette
+  lda #<inventory_screen_palette
+  sta w0
+  lda #>inventory_screen_palette
+  sta w0+1
+  jsr ppu_fade_in_palette
+
+  lda #<ppu_inventory_vblank
+  sta vblank_routine
+  lda #>ppu_inventory_vblank
+  sta vblank_routine+1
+
+inventory_state_main:
+
+  wait_vblank_data_ready
+
+  jsr controller_read
+
+  jsr sprite_clear_all
+
+  switch_bank_ldy #INVENTORY_STATE_BANK
+  jsr update_cursor
+
+  jsr draw_cursor
+
+  ;test start button
+  lda buffer_controller+buttons::_start
+  and #%00000011
+  cmp #%00000001
+  beq inventory_state_exit
+
+  set_vblank_data_ready
+
+  jmp inventory_state_main
+
+inventory_state_exit:
+
+  ;fade out inventory palette
+  lda #<inventory_screen_palette
+  sta w0
+  lda #>inventory_screen_palette
+  sta w0+1
+  jsr ppu_fade_out_palette
+
+  jmp play_state_reload
+
+.proc ppu_inventory_vblank
+
+  jsr sprite_update_all
+
+  lda #0
+  sta vblank_data_ready
+
+  rts
+
+.endproc
+
+.proc draw_cursor
+  ldy #sprite_chr_group_index_cursor
+  lda sprite_chr_group_offsets,y
+  sta sprite_group_offset
+
+  switch_bank_ldy #INVENTORY_STATE_SPRITES_AND_ANIMATIONS_BANK
+  lda #<Cursor0
+  sta w0
+  lda #>Cursor0
+  sta w0+1
+
+  lda state_control_params+inventory_state_control::cursor_column
+  sta w3
+  lda #0
+  sta w3+1
+
+  lda state_control_params+inventory_state_control::cursor_row
+  sta w4
+  lda #0
+  sta w4+1
+
+  lda #0
+  sta b2
+
+  jsr sprite_draw_metasprite
+
+  rts
+
+.endproc
+
+.segment "ROM03"
+
 ;inventory title string
 inventory_string: .byte I,N,V,E,N,T,O,R,_Y,ES
 
@@ -47,21 +288,6 @@ carry_adlanniel_string: .byte C,_A,R,R,_Y,SP,_A,D,L,_A,N,N,I,E,L,ES
 confuse_string: .byte C,O,N,F,U,S,E,ES
 homing_string: .byte H,O,M,I,N,G,ES
 multi_homing_string: .byte M,U,L,T,I,SP,H,O,M,I,N,G,ES
-
-inventory_screen_palette:
-  .byte $0d,$0d,$18,$20,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-  .byte $0d,$0d,$0d,$20,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-
-.proc ppu_inventory_vblank
-
-  jsr sprite_update_all
-
-  lda #0
-  sta vblank_data_ready
-
-  rts
-
-.endproc
 
 .proc update_cursor
 
@@ -178,38 +404,6 @@ done:
 
   pla
   tax
-  rts
-
-.endproc
-
-.proc draw_cursor
-
-  ldy #sprite_chr_group_index_cursor
-  lda sprite_chr_group_offsets,y
-  sta sprite_group_offset
-
-  switch_bank_ldy #INVENTORY_STATE_SPRITES_AND_ANIMATIONS_BANK
-  lda #<Cursor0
-  sta w0
-  lda #>Cursor0
-  sta w0+1
-
-  lda state_control_params+inventory_state_control::cursor_column
-  sta w3
-  lda #0
-  sta w3+1
-
-  lda state_control_params+inventory_state_control::cursor_row
-  sta w4
-  lda #0
-  sta w4+1
-
-  lda #0
-  sta b2
-
-draw_next_heart:
-  jsr sprite_draw_metasprite
-
   rts
 
 .endproc
@@ -433,195 +627,3 @@ menu_position_next_down:
 .proc indirect_jsr_w0
   jmp (w0)
 .endproc
-
-inventory_state_init:
-
-  ;fade out from current palette
-  switch_bank_ldy #AREAS_BANK
-  ldy #area::palette_address
-  lda (area_address),y
-  sta w0
-  iny
-  lda (area_address),y
-  sta w0+1
-  switch_bank_ldy map_bank
-  jsr ppu_fade_out_palette
-
-  ;set blank nmi routine
-  lda #<ppu_vblank_nop
-  sta vblank_routine
-  lda #>ppu_vblank_nop
-  sta vblank_routine+1
-
-  jsr ppu_safely_disable_graphics
-
-  jsr sprite_clear_all
-
-  jsr sprite_update_all
-
-  ;load chr data for inventory screen
-  lda #$00
-  sta $2006
-  sta $2006
-
-  ;reset tile accumulator
-  lda #$00
-  sta b3
-
-  lda #<inventory_chr
-  sta w0
-  lda #>inventory_chr
-  sta w0+1
-  switch_bank_ldy #INVENTORY_STATE_BG_CHR_BANK
-  jsr ppu_load_chr_amount
-
-  ;grab tile accumulator to know where the textbox and font group begins
-  lda b3
-  sta textbox_and_font_chr_offset
-
-  ;load the textbox graphics. This is hardcoded because it is the same
-  ;for the entire game. The assumption here is that the background
-  ;graphics we use will never occupy so many tiles that we cannot
-  ;display a textbox or font.
-  lda #<textbox_chr
-  sta w0
-  lda #>textbox_chr
-  sta w0+1
-  switch_bank_ldy #TEXTBOX_BG_CHR_BANK
-  jsr ppu_load_chr_amount
-
-  ;load cursor graphics
-  lda #$10
-  sta $2006
-  lda #$00
-  sta $2006
-
-  ;reset tile accumulator
-  lda #$00
-  sta b3
-
-  lda #<Cursor_chr
-  sta w0
-  lda #>Cursor_chr
-  sta w0+1
-
-  ;store the current chr offset in sprite_chr_groups_chr_offsets array for the cursor
-  ldy #sprite_chr_group_index_cursor
-  lda b3
-  sta sprite_chr_group_offsets,y
-
-  switch_bank_ldy #INVENTORY_STATE_SPRITE_CHR_BANK
-  jsr ppu_load_chr_amount
-
-  ;load nametable data for inventory screen
-  ;load the nametable and attribute table.
-  lda #$20
-  sta ppu_2006
-  lda #$00
-  sta ppu_2006+1
-  upload_ppu_2006
-  lda #<inventory_screen
-  sta w0
-  lda #>inventory_screen
-  sta w0+1
-  switch_bank_ldy #INVENTORY_STATE_BG_NAMETABLE_BANK
-  jsr ppu_load_nametable
-
-  ;reset scroll
-  lda #$20
-  sta ppu_2006
-  lda #$00
-  sta ppu_2006
-  upload_ppu_2006
-
-  lda #0
-  sta ppu_2005
-  sta ppu_2005+1
-  upload_ppu_2005
-
-  ;initialize and draw initial cursor
-  lda #menu_position_health
-  sta state_control_params+inventory_state_control::menu_position
-
-  ldx state_control_params+inventory_state_control::menu_position
-  lda menu_position_row,x
-  sta state_control_params+inventory_state_control::cursor_row
-  lda menu_position_column,x
-  sta state_control_params+inventory_state_control::cursor_column
-
-  jsr draw_cursor
-
-  jsr sprite_update_all
-
-  ;draw menu layout with strings
-  print_string inventory_string, #$20, #4, #11
-
-  print_string gp_string, #$20, #7, #4
-  print_string keys_string, #$20, #8, #4
-
-  print_string use_item_string, #$20, #10, #4
-  print_string health_string, #$20, #10, #13
-  print_string owl_health_string, #$20, #11, #13
-  print_string rope_string, #$20, #12, #13
-
-  print_string carry_string, #$20, #14, #4
-  print_string bomb_string, #$20, #14, #13
-  print_string lantern_string, #$20, #15, #13
-
-  print_string tech_string, #$20, #17, #4
-  print_string rush_string, #$20, #17, #13
-  print_string fetch_string, #$20, #18, #13
-  print_string sonar_string, #$20, #19, #13
-  print_string carry_item_string, #$20, #20, #13
-  print_string carry_adlanniel_string, #$20, #21, #13
-  print_string confuse_string, #$20, #22, #13
-  print_string homing_string, #$20, #23, #13
-  print_string multi_homing_string, #$20, #24, #13
-
-  jsr ppu_safely_enable_graphics
-
-  ;fade in inventory screen palette
-  lda #<inventory_screen_palette
-  sta w0
-  lda #>inventory_screen_palette
-  sta w0+1
-  jsr ppu_fade_in_palette
-
-  lda #<ppu_inventory_vblank
-  sta vblank_routine
-  lda #>ppu_inventory_vblank
-  sta vblank_routine+1
-
-inventory_state_main:
-
-  wait_vblank_data_ready
-
-  jsr controller_read
-
-  jsr sprite_clear_all
-
-  jsr update_cursor
-
-  jsr draw_cursor
-
-  ;test start button
-  lda buffer_controller+buttons::_start
-  and #%00000011
-  cmp #%00000001
-  beq inventory_state_exit
-
-  set_vblank_data_ready
-
-  jmp inventory_state_main
-
-inventory_state_exit:
-
-  ;fade out inventory palette
-  lda #<inventory_screen_palette
-  sta w0
-  lda #>inventory_screen_palette
-  sta w0+1
-  jsr ppu_fade_out_palette
-
-  jmp play_state_reload
-
