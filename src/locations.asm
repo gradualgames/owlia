c83809bf6f3d1f1035b7939c536a26c72a447af2
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2013-11-10 23:25:20 +0000

    Adding door type enum and door_constants file. Changing all doors in first dungeon to be DOOR_TYPE_UNLOCKED. DOOR_TYPE_KEYED activates the functionality we already have in place for using keys, and DOOR_TYPE_LOCKED remains locked until an external entity changes the type to DOOR_TYPE_UNLOCKED (this will be done by trap rooms or other entities that meet some condition or other and unlock a door).
    git-svn-id: svn://localhost/Owlia/trunk@503 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/locations.asm
+++ src/locations.asm
@@ -7,6 +7,7 @@
 .include "hero_constants.inc"
 .include "item_constants.inc"
 .include "npc_constants.inc"
+.include "door_constants.inc"
 .include "sprite_chr_data.inc"
 .include "conversation_data.inc"
 .include "entities.inc"
@@ -176,96 +177,96 @@ meadow3_entity_instances:
 dungeon_0_0_entity_instances:
   .byte 2
   .byte entity_index_door, 11, 13, 0, 2, location_index_dungeon_0_1_n, 0
-  .byte entity_index_door, 14, 7, 0, 2, location_index_dungeon_1_0_w, 0
+  .byte entity_index_door, 14, 7, 0, 2, location_index_dungeon_1_0_w, DOOR_TYPE_UNLOCKED
 
 dungeon_1_0_entity_instances:
   .byte 2
-  .byte entity_index_door, 17, 7, 0, 2, location_index_dungeon_0_0_e, 0
-  .byte entity_index_door, 30, 7, 0, 2, location_index_dungeon_2_0_w, 0
+  .byte entity_index_door, 17, 7, 0, 2, location_index_dungeon_0_0_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 30, 7, 0, 2, location_index_dungeon_2_0_w, DOOR_TYPE_UNLOCKED
 
 dungeon_2_0_entity_instances:
   .byte 4
   .byte entity_index_pufferfish, 39, 6, sprite_chr_group_index_pufferfish, 0
   .byte entity_index_pufferfish, 39, 10, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 33, 7, 0, 2, location_index_dungeon_1_0_e, 0
-  .byte entity_index_door, 46, 7, 0, 2, location_index_dungeon_3_0_w, 0
+  .byte entity_index_door, 33, 7, 0, 2, location_index_dungeon_1_0_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 46, 7, 0, 2, location_index_dungeon_3_0_w, DOOR_TYPE_UNLOCKED
 
 dungeon_3_0_entity_instances:
   .byte 1
-  .byte entity_index_door, 49, 7, 0, 2, location_index_dungeon_2_0_e, 0
+  .byte entity_index_door, 49, 7, 0, 2, location_index_dungeon_2_0_e, DOOR_TYPE_UNLOCKED
 
 dungeon_0_1_entity_instances:
   .byte 5
   .byte entity_index_pufferfish, 4, 22, sprite_chr_group_index_pufferfish, 0
   .byte entity_index_pufferfish, 11, 22, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 11, 18, 0, 2, location_index_dungeon_0_0_s, 0
-  .byte entity_index_door, 14, 22, 0, 2, location_index_dungeon_1_1_w, 0
-  .byte entity_index_door, 4, 28, 0, 2, location_index_dungeon_0_2_n, 0
+  .byte entity_index_door, 11, 18, 0, 2, location_index_dungeon_0_0_s, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 14, 22, 0, 2, location_index_dungeon_1_1_w, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 4, 28, 0, 2, location_index_dungeon_0_2_n, DOOR_TYPE_UNLOCKED
 
 dungeon_1_1_entity_instances:
   .byte 2
-  .byte entity_index_door, 17, 22, 0, 2, location_index_dungeon_0_1_e, 0
-  .byte entity_index_door, 30, 22, 0, 2, location_index_dungeon_2_1_w, 0
+  .byte entity_index_door, 17, 22, 0, 2, location_index_dungeon_0_1_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 30, 22, 0, 2, location_index_dungeon_2_1_w, DOOR_TYPE_UNLOCKED
 
 dungeon_2_1_entity_instances:
   .byte 3
   .byte entity_index_pufferfish, 39, 23, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 33, 22, 0, 2, location_index_dungeon_1_1_e, 0
-  .byte entity_index_door, 46, 22, 0, 2, location_index_dungeon_3_1_w, 0
+  .byte entity_index_door, 33, 22, 0, 2, location_index_dungeon_1_1_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 46, 22, 0, 2, location_index_dungeon_3_1_w, DOOR_TYPE_UNLOCKED
 
 dungeon_3_1_entity_instances:
   .byte 3
   .byte entity_index_pufferfish, 56, 23, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 49, 22, 0, 2, location_index_dungeon_2_1_e, 0
-  .byte entity_index_door, 56, 28, 0, 2, location_index_dungeon_3_2_n, 0
+  .byte entity_index_door, 49, 22, 0, 2, location_index_dungeon_2_1_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 56, 28, 0, 2, location_index_dungeon_3_2_n, DOOR_TYPE_UNLOCKED
 
 dungeon_0_2_entity_instances:
   .byte 3
   .byte entity_index_pufferfish, 7, 38, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 4, 33, 0, 2, location_index_dungeon_0_1_s, 0
-  .byte entity_index_door, 7, 43, 0, 2, location_index_dungeon_0_3_n, 0
+  .byte entity_index_door, 4, 33, 0, 2, location_index_dungeon_0_1_s, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 7, 43, 0, 2, location_index_dungeon_0_3_n, DOOR_TYPE_UNLOCKED
 
 dungeon_1_2_entity_instances:
   .byte 3
   .byte entity_index_pufferfish, 23, 37, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 26, 43, 0, 2, location_index_dungeon_1_3_n, 0
-  .byte entity_index_door, 30, 37, 0, 2, location_index_dungeon_2_2_w, 0
+  .byte entity_index_door, 26, 43, 0, 2, location_index_dungeon_1_3_n, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 30, 37, 0, 2, location_index_dungeon_2_2_w, DOOR_TYPE_UNLOCKED
 
 dungeon_2_2_entity_instances:
   .byte 4
   .byte entity_index_pufferfish, 38, 36, sprite_chr_group_index_pufferfish, 0
   .byte entity_index_pufferfish, 38, 40, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 33, 37, 0, 2, location_index_dungeon_1_2_e, 0
-  .byte entity_index_door, 46, 37, 0, 2, location_index_dungeon_3_2_w, 0
+  .byte entity_index_door, 33, 37, 0, 2, location_index_dungeon_1_2_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 46, 37, 0, 2, location_index_dungeon_3_2_w, DOOR_TYPE_UNLOCKED
 
 dungeon_3_2_entity_instances:
   .byte 2
-  .byte entity_index_door, 49, 37, 0, 2, location_index_dungeon_2_2_e, 0
-  .byte entity_index_door, 56, 33, 0, 2, location_index_dungeon_3_1_s, 0
+  .byte entity_index_door, 49, 37, 0, 2, location_index_dungeon_2_2_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 56, 33, 0, 2, location_index_dungeon_3_1_s, DOOR_TYPE_UNLOCKED
 
 dungeon_0_3_entity_instances:
   .byte 4
   .byte entity_index_pufferfish, 4, 52, sprite_chr_group_index_pufferfish, 0
   .byte entity_index_pufferfish, 11, 52, sprite_chr_group_index_pufferfish, 0
   .byte entity_index_crab, 11, 53, 0, 0
-  .byte entity_index_door, 7, 48, 0, 2, location_index_dungeon_0_2_s, 0
+  .byte entity_index_door, 7, 48, 0, 2, location_index_dungeon_0_2_s, DOOR_TYPE_UNLOCKED
 
 dungeon_1_3_entity_instances:
   .byte 3
   .byte entity_index_pufferfish, 22, 53, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 26, 48, 0, 2, location_index_dungeon_1_2_s, 0
-  .byte entity_index_door, 30, 52, 0, 2, location_index_dungeon_2_3_w, 0
+  .byte entity_index_door, 26, 48, 0, 2, location_index_dungeon_1_2_s, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 30, 52, 0, 2, location_index_dungeon_2_3_w, DOOR_TYPE_UNLOCKED
 
 dungeon_2_3_entity_instances:
   .byte 3
   .byte entity_index_pufferfish, 39, 53, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 33, 52, 0, 2, location_index_dungeon_1_3_e, 0
-  .byte entity_index_door, 46, 52, 0, 2, location_index_dungeon_3_3_w, 0
+  .byte entity_index_door, 33, 52, 0, 2, location_index_dungeon_1_3_e, DOOR_TYPE_UNLOCKED
+  .byte entity_index_door, 46, 52, 0, 2, location_index_dungeon_3_3_w, DOOR_TYPE_UNLOCKED
 
 dungeon_3_3_entity_instances:
   .byte 2
   .byte entity_index_pufferfish, 56, 53, sprite_chr_group_index_pufferfish, 0
-  .byte entity_index_door, 49, 52, 0, 2, location_index_dungeon_2_3_e, 0
+  .byte entity_index_door, 49, 52, 0, 2, location_index_dungeon_2_3_e, DOOR_TYPE_UNLOCKED
 
 ;****************************************************************
 ;Palettes.
