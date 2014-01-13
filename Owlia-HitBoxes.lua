75a80510d2af639aaee18bdbc5e47f9611a57410
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2014-01-13 00:25:12 +0000

    More updates to Owlia-HitBoxes.lua. Allows rects to partially show on the screen.
    git-svn-id: svn://localhost/Owlia/trunk@629 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- Owlia-HitBoxes.lua
+++ Owlia-HitBoxes.lua
@@ -17,6 +17,10 @@ b4 = 0x0004
 b5 = 0x0005
 w4 = 0x0014
 w5 = 0x0016
+w6 = 0x0018
+w7 = 0x001a
+w8 = 0x001c
+w9 = 0x001e
 
 camera_x = 0x05e7
 camera_y = 0x05e9
@@ -26,39 +30,68 @@ entity_action_rect1_y = 0x05ab
 entity_action_rect1_width = 0x05ad
 entity_action_rect1_height = 0x05ae
 
-geotests_rect_in_rect = 0xdec1
+geotests_rect_in_rect = 0xdef5
 
 -- draw a box and take care of coordinate checking
 local function box(x1,y1,x2,y2,color)
     y1 = y1 + 16;
     y2 = y2 + 16;
-    -- gui.text(50,50,x1..","..y1.." "..x2..","..y2);
-    if (x1 >= 0 and x1 <= 255 and x2 >= 0 and x2 <= 255 and y1 >= 0 and y1 < 224 and y2 >= 0 and y2 < 224) then
-        --gui.drawbox(x1,y1,x2,y2,color);
-        gui.drawline(x1,y1,x2,y1,color);
-        gui.drawline(x2,y1,x2,y2,color);
-        gui.drawline(x1,y2,x2,y2,color);
-        gui.drawline(x1,y1,x1,y2,color);
+
+    if (x1 > 255) then
+        return;
     end;
+
+    if (x2 < 0) then
+        return;
+    end;
+
+    if (y2 < 0) then
+        return;
+    end;
+
+    if (y1 > 239) then
+        return;
+    end;
+
+    if (x1 < 0) then
+        x1 = 0
+    end;
+
+    if (x2 > 255) then
+        x2 = 255
+    end;
+
+    if (y1 < 0) then
+        y1 = 0
+    end;
+
+    if (y2 > 239) then
+        y2 = 239
+    end;
+
+    gui.drawline(x1,y1,x2,y1,color);
+    gui.drawline(x2,y1,x2,y2,color);
+    gui.drawline(x1,y2,x2,y2,color);
+    gui.drawline(x1,y1,x1,y2,color);
 end;
 
-local function draw_attack_rect()
+-- local function draw_attack_rect()
 
-    camera_x16 = memory.readbyte(camera_x+1) * 256 + memory.readbyte(camera_x);
-    camera_y16 = memory.readbyte(camera_y+1) * 256 + memory.readbyte(camera_y);
+    -- camera_x16 = memory.readbyte(camera_x+1) * 256 + memory.readbyte(camera_x);
+    -- camera_y16 = memory.readbyte(camera_y+1) * 256 + memory.readbyte(camera_y);
 
-    entity_action_rect1_x16 = memory.readbyte(entity_action_rect1_x+1) * 256 + memory.readbyte(entity_action_rect1_x);
-    entity_action_rect1_y16 = memory.readbyte(entity_action_rect1_y+1) * 256 + memory.readbyte(entity_action_rect1_y);
+    -- entity_action_rect1_x16 = memory.readbyte(entity_action_rect1_x+1) * 256 + memory.readbyte(entity_action_rect1_x);
+    -- entity_action_rect1_y16 = memory.readbyte(entity_action_rect1_y+1) * 256 + memory.readbyte(entity_action_rect1_y);
 
-    screen_x = entity_action_rect1_x16 - camera_x16;
-    screen_y = entity_action_rect1_y16 - camera_y16;
+    -- screen_x = entity_action_rect1_x16 - camera_x16;
+    -- screen_y = entity_action_rect1_y16 - camera_y16;
 
-    entity_action_rect1_width_value = memory.readbyte(entity_action_rect1_width);
-    entity_action_rect1_height_value = memory.readbyte(entity_action_rect1_height);
+    -- entity_action_rect1_width_value = memory.readbyte(entity_action_rect1_width);
+    -- entity_action_rect1_height_value = memory.readbyte(entity_action_rect1_height);
 
-    box(screen_x, screen_y, screen_x+entity_action_rect1_width_value, screen_y+entity_action_rect1_height_value, "green");
+    -- box(screen_x, screen_y, screen_x+entity_action_rect1_width_value, screen_y+entity_action_rect1_height_value, "green");
 
-end;
+-- end;
 
 local function rect_in_rect_executed()
 
@@ -83,23 +116,23 @@ local function rect_in_rect_executed()
     camera_x16 = memory.readbyte(camera_x+1) * 256 + memory.readbyte(camera_x);
     camera_y16 = memory.readbyte(camera_y+1) * 256 + memory.readbyte(camera_y);
 
-    rect1_x16 = memory.readbyte(w2+1) * 256 + memory.readbyte(w2) - camera_x16;
-    rect1_y16 = memory.readbyte(w3+1) * 256 + memory.readbyte(w3) - camera_y16;
-    rect1_width = memory.readbyte(b2)
-    rect1_height = memory.readbyte(b3)
+    rect1_x16_left = memory.readbyte(w2+1) * 256 + memory.readbyte(w2) - camera_x16;
+    rect1_y16_top = memory.readbyte(w3+1) * 256 + memory.readbyte(w3) - camera_y16;
+    rect1_x16_right = memory.readbyte(w6+1) * 256 + memory.readbyte(w6) - camera_x16;
+    rect1_y16_bottom = memory.readbyte(w7+1) * 256 + memory.readbyte(w7) - camera_y16;
 
-    box(rect1_x16, rect1_y16, rect1_x16 + rect1_width, rect1_y16 + rect1_height, "red")
+    box(rect1_x16_left, rect1_y16_top, rect1_x16_right, rect1_y16_bottom, "red")
 
-    rect2_x16 = memory.readbyte(w4+1) * 256 + memory.readbyte(w4) - camera_x16;
-    rect2_y16 = memory.readbyte(w5+1) * 256 + memory.readbyte(w5) - camera_y16;
-    rect2_width = memory.readbyte(b4)
-    rect2_height = memory.readbyte(b5)
+    rect2_x16_left = memory.readbyte(w4+1) * 256 + memory.readbyte(w4) - camera_x16;
+    rect2_y16_top = memory.readbyte(w5+1) * 256 + memory.readbyte(w5) - camera_y16;
+    rect2_x16_right = memory.readbyte(w8+1) * 256 + memory.readbyte(w8) - camera_x16;
+    rect2_y16_bottom = memory.readbyte(w9+1) * 256 + memory.readbyte(w9) - camera_y16;
 
-    box(rect2_x16, rect2_y16, rect2_x16 + rect2_width, rect2_y16 + rect2_height, "red")
+    box(rect2_x16_left, rect2_y16_top, rect2_x16_right, rect2_y16_bottom, "red")
 
 end;
 
-memory.registerexecute(geotests_rect_in_rect, 110, rect_in_rect_executed)
+memory.registerexecute(geotests_rect_in_rect, 59, rect_in_rect_executed)
 
 local a,b,c,d;
 while (running) do
