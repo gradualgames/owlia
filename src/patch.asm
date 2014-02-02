8febcd0aff16f72eb237114b5f12a0a08333943c
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2014-02-02 01:10:00 +0000

    Implementing decode_nametable_patch_column. Implementing new state machine in monolith entity to perform a falling animation using column decode instead of row decode. The monolith entity can now be initialized to fall using rows or fall using columns. Positioned properly, this allows us to animate four monoliths at the same time.
    git-svn-id: svn://localhost/Owlia/branches/new_dungeon_exits_approach2@669 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/patch.asm
+++ src/patch.asm
@@ -7,11 +7,6 @@
 
 .segment "CODE"
 
-; -a routine which takes an x and a y coordinate, and decodes a row of the
-; map, but then writes a row of a given nametable patch overtop of the
-; decoded nametable buffer. There would need to be a row offset for where
-; to begin drawing the row of the given nametable patch.
-
 ;expects w0 to be the map x coordinate at which to decode the row
 ;expects w1 to be the map y coordinate at which to decode the row
 ;expects b0 to be the index of the nametable patch out of which a row will be decoded
@@ -59,7 +54,9 @@ patch_address = w3
   pha
 
   ;temporarily modify map_x to be camera_x. This is so that we decode
-  ;a full row that fits the current screen, not bleeding into opposing nametables.
+  ;a full row that fits the current screen, not bleeding into opposing nametables,
+  ;and so that if there are more than one patch entity in the same row, they can
+  ;rely on row offsets being relative to the same position.
   lda camera_x
   sta map_x
   lda camera_x+1
@@ -146,6 +143,189 @@ skip_row_decode:
 
   lda #1
   sta row_ready
+
+  ;restore calling bank
+  pla
+  sta current_bank
+  switch_bank_ldy current_bank
+
+  pla
+  tax
+
+  rts
+
+.endproc
+
+;expects w0 to be the map x coordinate at which to decode the column
+;expects w1 to be the map y coordinate at which to decode the column
+;expects b0 to be the index of the nametable patch out of which a column will be decoded
+;expects b1 to be the column of the patch to use
+;expects b2 to be height of the column to copy
+.proc decode_nametable_patch_column
+;params
+map_x = w0
+map_y = w1
+patch_index = b0
+patch_column = b1
+patch_column_height = b2
+
+;locals
+column_offset = b3
+patch_width = b4
+patch_height = b5
+patch_offset = b6
+patches_address = w2
+patch_address = w3
+
+  txa
+  pha
+
+  ;save calling bank
+  lda current_bank
+  pha
+
+  ;skip decoding a map column if a column is already ready. This enables us to
+  ;perform patches across an entire column all during a single frame, such as
+  ;for monoliths that are vertically across from one another.
+  lda column_ready
+  bne skip_column_decode
+
+  ;save patch params, the map decode routines clobber a lot of zp state
+  lda map_x
+  pha
+  lda map_x+1
+  pha
+  lda map_y
+  pha
+  lda map_y+1
+  pha
+  lda patch_index
+  pha
+  lda patch_column
+  pha
+  lda patch_column_height
+  pha
+
+  ;temporarily modify map_y to be camera_y. This is so that we decode
+  ;a full column that fits the current screen, and if a second patch
+  ;entity in the same column chooses to patch more parts of the column,
+  ;it can rely on the offsets being relative to the same position.
+  lda camera_y
+  sta map_y
+  lda camera_y+1
+  sta map_y+1
+
+  far_call map_bank, map_decode_column
+  far_call map_bank, map_process_intermediate_attribute_column_buffer
+
+  ;restore patch params
+  pla
+  sta patch_column_height
+  pla
+  sta patch_column
+  pla
+  sta patch_index
+  pla
+  sta map_y+1
+  pla
+  sta map_y
+  pla
+  sta map_x+1
+  pla
+  sta map_x
+skip_column_decode:
+
+  ;transform map_y to metatile coordinates
+  lda map_y
+  lsr map_y+1
+  ror
+  lsr map_y+1
+  ror
+  lsr map_y+1
+  ror
+  pha  ;save the low byte here, as the low bit will be needed as an offset
+       ;from the value read from mod15lut. We can only compute the correct
+       ;nametable offset every 2 tiles from mod15lut because it was designed
+       ;for metatile units, not nametable units.
+  lsr map_y+1
+  ror
+  sta map_y
+
+  ;transform map_y to nametable coordinates using mod15lut
+  ldx map_y
+  lda mod15lut,x
+  asl
+  sta map_y
+
+  ;grab the low byte of map_x that we grabbed in the midst of dividing map_y
+  ;by 16 (right when we had it at divided by 8), and use the low bit as an offset
+  ;from the nametable coordinate we computed from the mod15lut, to finally get the
+  ;correct offset within the column in nametable units. Kind of a funky workaround
+  ;for the fact that we do not have a mod30lut, but I don't feel like putting another
+  ;chunky look up table in the fixed bank. :)
+  pla
+  and #1
+  clc
+  adc map_y
+  sta map_y
+
+  ;calculate column offset from map_y
+  sta column_offset
+
+  ;get address of patches array
+  switch_bank_ldy #AREAS_BANK
+  ldy #area::nametable_patches_address
+  lda (area_address),y
+  sta patches_address
+  iny
+  lda (area_address),y
+  sta patches_address+1
+
+  ;get address of patch
+  lda patch_index
+  asl
+  tay
+  lda (patches_address),y
+  sta patch_address
+  iny
+  lda (patches_address),y
+  sta patch_address+1
+
+  ;get width and height of patch
+  ldy #0
+  lda (patch_address),y
+  sta patch_width
+  iny
+  lda (patch_address),y
+  sta patch_height
+
+  ;start at first row of column (offset by 2 to skip width and height header)
+  lda #2
+  sta patch_offset
+
+  ;start at the column of the patch that we selected
+  clc
+  lda patch_offset
+  adc patch_column
+  sta patch_offset
+
+  ldx column_offset
+: ldy patch_offset
+  lda (patch_address),y
+  sta nametable_column_buffer,x
+
+  ;advance to next row of column that we are copying
+  clc
+  lda patch_offset
+  adc patch_width
+  sta patch_offset
+
+  inx
+  dec patch_column_height
+  bne :-
+
+  lda #1
+  sta column_ready
 
   ;restore calling bank
   pla
