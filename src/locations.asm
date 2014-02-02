c6a91cf72ae004daa78544df4c774008dd2be3e1
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2014-02-02 15:53:23 +0000

    Implementing rising monolith animation using row patching.
    git-svn-id: svn://localhost/Owlia/branches/new_dungeon_exits_approach2@671 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/locations.asm
+++ src/locations.asm
@@ -254,10 +254,10 @@ meadow3_entity_instances:
 
 dungeon_0_0_entity_instances:
   .byte 4
-  .byte entity_index_monolith, 1, 8, 0, 0
-  .byte entity_index_monolith, 7, 3, 0, 0
-  .byte entity_index_monolith, 14, 8, 0, 0
-  .byte entity_index_monolith, 7, 12, 0, 0
+  .byte entity_index_monolith, 1, 8, 0, MONOLITH_PARAMS, 1, 4, MONOLITH_STATE_RISE_USING_ROWS_INIT
+  .byte entity_index_monolith, 14, 8, 0, MONOLITH_PARAMS, 1, 5, MONOLITH_STATE_RISE_USING_ROWS_INIT
+  .byte entity_index_monolith, 7, 3, 0, MONOLITH_PARAMS, 1, 2, MONOLITH_STATE_IDLE
+  .byte entity_index_monolith, 7, 12, 0, MONOLITH_PARAMS, 1, 3, MONOLITH_STATE_IDLE
 
 dungeon_1_0_entity_instances:
   .byte 2
