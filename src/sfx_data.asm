69949ccd6f7c354c28a687af0f6cf6c7bf023b43
derek <derek@c1cc9e3d-c02f-ee42-9970-117c3f37ddad>
2014-08-16 22:17:52 +0000

    Integrating the puzzle solved melody. Had to introduce a delay into the spotlight puzzle (where it is currently used) so that explosion sound effects from the anglerfish could complete.
    git-svn-id: svn://localhost/Owlia/trunk@868 c1cc9e3d-c02f-ee42-9970-117c3f37ddad

--- src/sfx_data.asm
+++ src/sfx_data.asm
@@ -79,6 +79,10 @@ sfx_volume_envelope_17:
 sfx_volume_envelope_18:
   .byte 3,4,6,7,9,9,10,11,11,12,12,12,12,13,13,13,13,13,14,14,11,8,3,2,0,ENV_STOP
 
+;used for solved puzzle sound
+sfx_volume_envelope_19:
+  .byte 3,4,6,7,9,9,10,11,11,12,12,12,12,13,13,13,13,13,14,14,11,8,3,2,0,ENV_STOP
+
 sfx_pitch_envelope_0:
   .byte 0, ENV_LOOP
 
@@ -114,6 +118,7 @@ sfx_volume_envelopes:
   .word sfx_volume_envelope_16
   .word sfx_volume_envelope_17
   .word sfx_volume_envelope_18
+  .word sfx_volume_envelope_19
 
 sfx_pitch_envelopes:
   .word sfx_pitch_envelope_0
@@ -192,4 +197,14 @@ sfx_chest_open:
 sfx_chest_melody:
   .byte STV,18,STP,2,SDU,1,STL,4,C2,D2,E2,FS2,GS2,AS2,C3,E3,GS3,STL,32
   .byte C4
+  .byte TRM
+
+sfx_solved_puzzle_instrument_one:
+  .byte STV,19,STP,2,SDU,1,STL,6,C3,G3,E3,G3,F3,C4,A3,C4,G3,D4,B3
+  .byte F4,STL,32,E4
+  .byte TRM
+
+sfx_solved_puzzle_instrument_two:
+  .byte STV,19,STP,2,SDU,1,STL,6,E3,C4,G3,C4,A3,F4,C4,F4,D4,B4,F4
+  .byte B4,STL,32,C5
   .byte TRM
