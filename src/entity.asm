867078569448482d9eb39880665f44f756b4764d
Derek Andrews <derek.george.andrews@gmail.com>
2014-12-23 16:36:13 -0500

    Revert "Remove entity screen clipping for now---it is messing up the appearance of the swordfish boss. Hopefully entity pausing is enough of a performance boost on overworld maps this shouldn't really matter anymore."
    This reverts commit 2862ec06dd5da8e072e3faeb80347df4e23579e7.

--- src/entity.asm
+++ src/entity.asm
@@ -1226,6 +1226,10 @@ draw_entity:
   lda entity_flags,x
   and #ENTITY_FLAGS_ALIVE_TEST
   beq skip_entity
+  ;if we arrive here, we've found a living entity. Clip it against the screen.
+
+  jsr entity_test_screen_rect
+  bne skip_entity
 
   ;test to see if it is drawable.
   lda entity_flags,x
