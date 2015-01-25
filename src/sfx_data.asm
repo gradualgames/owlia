02a867c7db8a4d2a80264b9ced826306b0142f19
Derek Andrews <derek.george.andrews@gmail.com>
2015-01-25 15:16:13 -0500

    Refactoring pitch and duty envelopes to use human readable enumerated indices as well as the volume envelopes. The volume envelopes are most closely related to the character of the sound effects with which they are used, so the pitch and duty envelopes have names that describe their function.

--- src/sfx_data.asm
+++ src/sfx_data.asm
@@ -4,6 +4,10 @@
 
 .segment "ROM06"
 
+;****************************************************************
+;volume envelopes
+;****************************************************************
+
 sfx_volume_envelope_silence:
   .byte 0, ENV_STOP
 
@@ -67,21 +71,31 @@ sfx_volume_envelope_ice_explosion:
 sfx_volume_envelope_whoosh:
   .byte 1,2,2,3,4,4,4,4,5,5,6,6,6,7,8,8,8,8,7,7,7,7,7,7,7,6,6,6,6,6,6,6,5,5,5,5,5,5,4,4,4,4,4,4,4,4,3,3,3,3,2,2,2,2,2,2,2,1,1,1,0,0,0,0,ENV_STOP
 
+;****************************************************************
+;pitch envelopes
+;****************************************************************
+
 sfx_pitch_envelope_flat:
   .byte 0, ENV_LOOP
 
-;used for get item melody
-sfx_pitch_envelope_2:
+sfx_pitch_envelope_vibrato:
   .byte 0,0,0,0,0,0,0,0,0,-1,-1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,1,1,ENV_STOP
 
-;used for octoboss punch
-sfx_pitch_envelope_3:
+sfx_pitch_envelope_long_slide_down:
   .byte 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,ENV_STOP
 
-sfx_duty_envelope_0:
+;****************************************************************
+;duty envelopes
+;****************************************************************
+
+sfx_duty_envelope_standard:
   .byte 0, DUTY_ENV_STOP
-sfx_duty_envelope_1:
+sfx_duty_envelope_ooo:
   .byte -128,DUTY_ENV_LOOP
+
+;****************************************************************
+;luts
+;****************************************************************
 
 sfx_volume_envelopes:
   .word sfx_volume_envelope_silence
@@ -108,83 +122,90 @@ sfx_volume_envelopes:
 
 sfx_pitch_envelopes:
   .word sfx_pitch_envelope_flat
-  .word 0
-  .word sfx_pitch_envelope_2
-  .word sfx_pitch_envelope_3
+  .word sfx_pitch_envelope_vibrato
+  .word sfx_pitch_envelope_long_slide_down
 
 sfx_duty_envelopes:
-  .word sfx_duty_envelope_0
-  .word sfx_duty_envelope_1
+  .word sfx_duty_envelope_standard
+  .word sfx_duty_envelope_ooo
+
+;****************************************************************
+;sound effects sets
+;****************************************************************
 
 sfx_set1:
   .word sfx_volume_envelopes
   .word sfx_pitch_envelopes
   .word sfx_duty_envelopes
 
+;****************************************************************
+;sound effect definitions
+;****************************************************************
+
 sfx_hit:
-  .byte STV,sfx_volume_envelope_index_hit,STP,0,SDU,0,STL,20,8
+  .byte STV,sfx_volume_envelope_index_hit,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,20,8
   .byte TRM
 
 sfx_sword:
-  .byte STV,sfx_volume_envelope_index_sword,STP,0,SDU,0,STL,6,8
+  .byte STV,sfx_volume_envelope_index_sword,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,6,8
   .byte TRM
 
 sfx_door:
-  .byte STV,sfx_volume_envelope_index_door,STP,0,SDU,0,STL,6,10,15
+  .byte STV,sfx_volume_envelope_index_door,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,6,10,15
   .byte TRM
 
 sfx_flap:
-  .byte STV,sfx_volume_envelope_index_flap,STP,0,SDU,0,STL,45,9
+  .byte STV,sfx_volume_envelope_index_flap,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,45,9
   .byte TRM
 
 sfx_explosion:
-  .byte STV,sfx_volume_envelope_index_explosion,STP,0,SDU,0,STL,2,3,4,6,8,10,11,STL,6,13
+  .byte STV,sfx_volume_envelope_index_explosion,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,2,3,4,6,8,10,11,STL,6,13
   .byte TRM
 
 sfx_text:
-  .byte STV,sfx_volume_envelope_index_text,STP,0,SDU,0,STL,3,F4
+  .byte STV,sfx_volume_envelope_index_text,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,3,F4
   .byte TRM
 
 sfx_get_item:
-  .byte STV,sfx_volume_envelope_index_get_item,STP,0,SDU,1,STL,3,D6,A6,STV,0,D6
+  .byte STV,sfx_volume_envelope_index_get_item,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_ooo,STL,3,D6,A6,STV,0,D6
   .byte TRM
 
 sfx_move_cursor:
-  .byte STV,sfx_volume_envelope_index_cursor,STP,0,SDU,0,STL,5,11
+  .byte STV,sfx_volume_envelope_index_cursor,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,5,11
   .byte TRM
 
 sfx_select:
-  .byte STV,sfx_volume_envelope_index_select,STP,0,SDU,1,STL,2,GS3,GS4,GS5
+  .byte STV,sfx_volume_envelope_index_select,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_ooo,STL,2,GS3,GS4,GS5
   .byte TRM
 
 sfx_inventory:
-  .byte STV,sfx_volume_envelope_index_inventory,STP,0,SDU,1,STL,6,G4,G3,G2,G1,G2,G3
+  .byte STV,sfx_volume_envelope_index_inventory,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_ooo,STL,6,G4,G3,G2,G1,G2,G3
   .byte TRM
 
 sfx_error:
-  .byte STV,sfx_volume_envelope_index_error,STP,0,SDU,0,STL,4,B1,F1
+  .byte STV,sfx_volume_envelope_index_error,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,4,B1,F1
   .byte TRM
 
 sfx_monolith_slam:
-  .byte STV,sfx_volume_envelope_index_monolith,STP,0,SDU,0,STL,24,15
+  .byte STV,sfx_volume_envelope_index_monolith,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,24,15
   .byte TRM
 
 sfx_chest_open:
-  .byte STV,sfx_volume_envelope_index_chest_open,SDU,0,STL,4,$6,$3
+  .byte STV,sfx_volume_envelope_index_chest_open,SDU,sfx_duty_envelope_index_standard,STL,4,$6,$3
   .byte TRM
 
 sfx_chest_melody:
-  .byte STV,sfx_volume_envelope_index_chest_melody,STP,2,SDU,1,STL,4,C2,D2,E2,FS2,GS2,AS2,C3,E3,GS3,STL,32
+  .byte STV,sfx_volume_envelope_index_chest_melody,STP,sfx_pitch_envelope_index_vibrato,SDU,sfx_duty_envelope_index_ooo,STL,4,C2,D2,E2,FS2,GS2,AS2,C3,E3,GS3,STL,32
   .byte C4
   .byte TRM
 
 sfx_solved_puzzle_instrument_one:
-  .byte STV,sfx_volume_envelope_index_solve_puzzle,STP,2,SDU,1,STL,6,C3,G3,E3,G3,F3,C4,A3,C4,G3,D4,B3
+  .byte STV,sfx_volume_envelope_index_solve_puzzle,STP,sfx_pitch_envelope_index_vibrato,SDU,sfx_duty_envelope_index_ooo,STL,6,C3,G3,E3,G3,F3,C4,A3,C4,G3,D4,B3
   .byte F4,STL,32,E4
   .byte TRM
 
 sfx_solved_puzzle_instrument_two:
-  .byte STV,sfx_volume_envelope_index_solve_puzzle,STP,2,SDU,1,STL,6,E3,C4,G3,C4,A3,F4,C4,F4,D4,B4,F4
+  .byte STV,sfx_volume_envelope_index_solve_puzzle,STP,sfx_pitch_envelope_index_vibrato,SDU,sfx_duty_envelope_index_ooo,STL,6,E3,C4,G3,C4,A3,F4,C4,F4,D4,B4,F4
   .byte B4,STL,32,C5
   .byte TRM
 
@@ -193,17 +214,17 @@ sfx_octoboss_rise:
   .byte TRM
 
 sfx_octoboss_punch:
-  .byte STV,sfx_volume_envelope_index_loud,STP,3,STL,50,C4
+  .byte STV,sfx_volume_envelope_index_loud,STP,sfx_pitch_envelope_index_long_slide_down,STL,50,C4
   .byte TRM
 
 sfx_ice_shatter_square:
-  .byte STV,sfx_volume_envelope_index_ice_explosion,STP,0,SDU,0,STL,2,C7,C7,AS7,F7,STL,16,A7
+  .byte STV,sfx_volume_envelope_index_ice_explosion,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,2,C7,C7,AS7,F7,STL,16,A7
   .byte TRM
 
 sfx_ice_shatter_noise:
-  .byte STV,sfx_volume_envelope_index_ice_explosion,STP,0,SDU,0,STL,16,$8
+  .byte STV,sfx_volume_envelope_index_ice_explosion,STP,sfx_pitch_envelope_index_flat,SDU,sfx_duty_envelope_index_standard,STL,16,$8
   .byte TRM
 
 sfx_whoosh:
-  .byte STV,sfx_volume_envelope_index_whoosh,STP,0,STL,64,$9
+  .byte STV,sfx_volume_envelope_index_whoosh,STP,sfx_pitch_envelope_index_flat,STL,64,$9
   .byte TRM
