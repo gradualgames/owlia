d672b21f05e4c58db6f7cd97c9a4fa0eec0f989f
Derek Andrews <derek.george.andrews@gmail.com>
2015-08-15 19:35:26 -0400

    Move eye and pincer spawning to init state so they are visible as the scene fades in. Adjust the state counters for the first time the crab begins moving so that we can spawn him closer to the center of the arena. Looks more natural.

--- src/locations.asm
+++ src/locations.asm
@@ -1859,7 +1859,7 @@ dungeon3_3_3_entity_instances:
 
 dungeon3_boss_entity_instances:
   .byte 4
-  .byte entity_index_crab_boss_body, 3, 6, 0, 0
+  .byte entity_index_crab_boss_body, 6, 6, 0, 0
   .byte entity_index_monolith, 14, 11, 0, MONOLITH_PARAMS, \
                                           MONOLITH_TYPE_LOCKED | MONOLITH_FLAGS_UP_SET, 0, \
                                           MONOLITH_DIRECTION_EAST, \
